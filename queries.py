0123c95dbe2d4a176a654ed8c3bea4101fd2c7f9
Prasant Kumar Mahato <prasantmahato33@gmail.com>
2024-02-05 03:42:31 +0530

    Module to store queries

---
+++ queries.py
@@ -0,0 +1,17 @@
+class Queries:
+    drop = "DROP TABLE IF EXISTS register"
+    
+    create = "CREATE TABLE register ( \
+        ID INT PRIMARY KEY AUTO_INCREMENT, \
+        Name VARCHAR(255) NOT NULL, \
+        Email VARCHAR(255) NOT NULL, \
+        dob DATE \
+    );"
+    
+    insert = "INSERT INTO register(name, email, dob) VALUES(%s, %s, %s)"
+    
+    update = "UPDATE register SET name=%s WHERE email=%s"
+    
+    delete = "DELETE FROM register WHERE email=%s"
+    
+    select = "SELECT * FROM register"
